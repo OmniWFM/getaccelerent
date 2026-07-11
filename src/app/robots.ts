d18d7d04cc4c97749e134635c0279eaf33e1beae
OmniWFM <samuel@treasureadvertising.com>
2026-07-11 18:37:06 -0500

    Add robots

---
+++ src/app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/", disallow: "/portal" },
+    sitemap: "https://getaccelerent.com/sitemap.xml",
+  };
+}
